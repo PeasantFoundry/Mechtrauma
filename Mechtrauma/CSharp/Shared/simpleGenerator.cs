040b6ef9fa6dc3eabb9547b5a7103741abe19a13
Torca <Torca2001@gmail.com>
2023-01-04 23:48:50 +1100

    Simple generator component Implemented a simple generator to be used by Lua to have stable power.

---
+++ Mechtrauma/CSharp/Shared/simpleGenerator.cs
@@ -0,0 +1,72 @@
+/***
+Simple generator component for Mechtrauma that allows for configurable power output and tolerance.
+Using negative 'PowerConsumption' variable to provide power to the grid. While positive to add a load.
+And the 'PowerTolerance' variable to allow for snapping to the grid demand.
+***/
+using System;
+using Barotrauma;
+using Barotrauma.Networking;
+using System.Reflection;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Barotrauma.Items.Components;
+using System.Linq;
+
+namespace Barotrauma.Items.Components 
+{
+    class SimpleGenerator : Powered {
+
+        public float PowerTolerance {
+            get => powerTolerance;
+            set => powerTolerance = value;
+        }
+        private float powerTolerance = 0.0f;
+
+        [Editable, Serialize(0.0f, IsPropertySaveable.Yes, description: "Configurable Maximum power output of the device", alwaysUseInstanceValues: true)]
+        public float MaxPowerOut {
+            get => maxPowerOut;
+            set => maxPowerOut = value;
+        }
+        private float maxPowerOut = 0.0f;
+
+        public SimpleGenerator(Item item, ContentXElement element) : base(item, element) {
+            IsActive = true;
+            PowerTolerance = 0.0f;
+        }
+
+        public override float GetCurrentPowerConsumption(Connection connection) {
+            if (connection == powerIn) {
+                return 0.0f;
+            }
+
+            return (PowerConsumption >= 0 ? PowerConsumption : -1.0f);
+        }
+
+        // Calculate the min and max power output of the device for the given tolerance
+        public override PowerRange MinMaxPowerOut(Connection connection, float load = 0) {
+            if (connection == powerOut) {
+                float minOut = -PowerConsumption * (1 - PowerTolerance);
+                float maxOut = -PowerConsumption * (1 + PowerTolerance);
+                return new PowerRange(minOut, maxOut, MaxPowerOut);
+            }
+            return PowerRange.Zero;
+        }
+
+        // Calculate power output alongside other simple generators in the same network to snap within their tolerances
+        public override float GetConnectionPowerOut(Connection connection, float power, PowerRange minMaxPower, float load) {
+            if (connection == powerOut && minMaxPower.Max > 0) {
+                PowerRange myRange = MinMaxPowerOut(connection, load);
+
+                float ratio = MathHelper.Max((load - power - minMaxPower.Min) / (minMaxPower.Max - minMaxPower.Min), 0);
+                if (float.IsInfinity(ratio))
+                {
+                    ratio = 0;
+                }
+
+                return MathHelper.Clamp(ratio * (myRange.Max - myRange.Min) + myRange.Min, myRange.Min, myRange.Max);
+            }
+            return 0.0f;
+        }
+
+    }
+}
