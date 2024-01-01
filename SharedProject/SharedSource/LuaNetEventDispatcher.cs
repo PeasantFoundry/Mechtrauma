9b5440e28062c1f0e9d92f84aa6e22c65cc8faa7
MapleWheels <njainanan@hotmail.com>
2024-01-01 03:34:40 -0500

    - Added lua net event dispatcher.

---
+++ SharedProject/SharedSource/LuaNetEventDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using Barotrauma;
+using Barotrauma.Items.Components;
+
+namespace Mechtrauma;
+
+public partial class LuaNetEventDispatcher : ItemComponent
+{
+    public static readonly string Event_ServerRead = "Mechtrauma.LuaNetEventDispatcher::ServerRead";
+    public static readonly string Event_ServerWrite = "Mechtrauma.LuaNetEventDispatcher::ServerWrite";
+    public static readonly string Event_ClientRead = "Mechtrauma.LuaNetEventDispatcher::ClientRead";
+    public static readonly string Event_ClientWrite = "Mechtrauma.LuaNetEventDispatcher::ClientWrite";
+    
+    public string Name { get; set; }
+    
+    public LuaNetEventDispatcher(Item item, ContentXElement element) : base(item, element)
+    {
+        InitializeXml(element);
+    }
+
+    public void InitializeXml(ContentXElement element)
+    {
+        if (element is null)
+            return;
+
+        Name = element.GetAttributeString("Name", string.Empty);
+    }
+
+    public virtual partial void SendEvent();
+}
