007a9545752317ca6bdcce97e374a52307c9ce06
MapleWheels <njainanan@hotmail.com>
2024-01-01 03:38:06 -0500

    - Added itemcomponent to args list.

--- ServerProject/ServerSource/LuaNetEventDispatcher.cs
+++ ServerProject/ServerSource/LuaNetEventDispatcher.cs
@@ -7,12 +7,12 @@ public partial class LuaNetEventDispatcher : IClientSerializable, IServerSeriali
 {
     public void ServerEventRead(IReadMessage msg, Client c)
     {
-        GameMain.LuaCs.Hook.Call(Event_ServerRead, msg, c);
+        GameMain.LuaCs.Hook.Call(Event_ServerRead, this, msg, c);
     }
 
     public void ServerEventWrite(IWriteMessage msg, Client c, NetEntityEvent.IData extraData = null)
     {
-        GameMain.LuaCs.Hook.Call(Event_ServerWrite, msg, c, extraData);
+        GameMain.LuaCs.Hook.Call(Event_ServerWrite, this, msg, c, extraData);
     }
     
     public virtual partial void SendEvent()
